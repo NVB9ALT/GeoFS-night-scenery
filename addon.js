965c70f89a000527164d1e5ef3582b8c28ee14d2
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-09-23 17:35:01 -0700

    Update addon.js

--- addon.js
+++ addon.js
@@ -1,1 +1,17 @@
-javascript: (() => {geofs.api.blackMarble = geofs.api.viewer.imageryLayers.addImageryProvider(new Cesium.UrlTemplateImageryProvider({ url: geofs.dataServer + "bm/{z}/{x}/{reverseY}.png", credit: "Black Marble imagery courtesy NASA Earth Observatory", maximumLevel: 8 }));geofs.api.blackMarble.alpha = 0.25;geofs.api.blackMarble.hue = 3.5;geofs.api.blackMarble.saturation = 0.7;geofs.api.blackMarble.brightness = 10;geofs.api.blackMarble.show = !1;function checkIsMarbleShow() {if (geofs.isNight == 1 && geofs.api.renderingSettings.advancedAtmosphere == 0) {geofs.api.setImageryBrightness(150);geofs.api.blackMarble.show = 1;};if (geofs.isNight == 0){geofs.api.blackMarble.show = 0;};if (geofs.isNight == 0 && weather.definition.cloudCover >= 80 && geofs.preferences.graphics.volumetricClouds == 0) {geofs.api.setImageryBrightness(1);};};checkMarbleInterval = setInterval(function(){checkIsMarbleShow()},100);})()
+function checkNightStuff() {
+   if (geofs.isNight == 1 && geofs.api.renderingSettings.advancedAtmosphere == 0 && geofs.camera.currentModeName !== "cockpit") {
+geofs.api.setImageryBrightness(75);
+   };
+   if (geofs.isNight == 1 && geofs.api.renderingSettings.advancedAtmosphere == 0 && geofs.camera.currentModeName == "cockpit") {
+geofs.api.setImageryBrightness(50);
+   };
+   if (geofs.isNight == 0 && weather.definition.cloudCover >= 80 && (geofs.preferences.graphics.volumetricClouds == false || geofs.api.renderingSettings.advancedAtmosphere == 0)) {
+geofs.api.viewer.scene.light.intensity = weather.definition.cloudCover / 80
+   };
+   if (geofs.camera.currentModeName == "cockpit" && geofs.isNight == 1) {
+geofs.api.viewer.scene.light.intensity = 0.5;
+geofs.api.viewer.scene.light.color = {red: 0.8, green: 0.63, blue: 0.52, alpha: 1};
+	} else if (geofs.isNight == 1) {
+geofs.api.viewer.scene.light.intensity = 0.1
+	}
+};checkMarbleInterval = setInterval(function(){checkNightStuff()},10)
